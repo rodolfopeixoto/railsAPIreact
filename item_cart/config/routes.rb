8417a8e1fa7e79d2f68610dc6fcd36a9900e18a0
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2016-10-10 21:57:15 -0300

    Added new methods and fixed url

--- item_cart/config/routes.rb
+++ item_cart/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-     resouces :items, only: [:index, :create, :destroy, :update]
+     resources :items, only: [:index, :create, :destroy, :update]
     end
   end
 end
